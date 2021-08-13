7be22ddaf539f9ff026498045303d1637ca3c8c7
Michal Svorc <dev@michalsvorc.com>
2021-08-13 17:04:26 +0200

    Neovim: update git realted keymap

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,4 @@
 -- https://github.com/sindrets/diffview.nvim
 
 -- Keymap
-vim.api.nvim_set_keymap('n', '<Leader>dd', ':DiffviewOpen<CR>', {noremap = true, silent = false})
-vim.api.nvim_set_keymap('n', '<Leader>dc', ':DiffviewClose<CR>', {noremap = true, silent = false})
+vim.api.nvim_set_keymap('n', '<Leader>gd', ':DiffviewOpen<CR>', {noremap = true, silent = false})
