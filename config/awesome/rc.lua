3be810f6c3815dc295cb78fecd3618ba3d2c5856
Michal Svorc <dev@michalsvorc.com>
2021-08-17 16:44:03 +0200

    awesome: switch keymap for screen navigation

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -323,13 +323,13 @@ globalkeys = gears.table.join(
   awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
     {description = "go back", group = "tag"}),
 
-  awful.key({ modkey,           }, "k",
+  awful.key({ modkey,           }, "l",
     function ()
       awful.client.focus.byidx( 1)
     end,
     {description = "focus next by index", group = "client"}
   ),
-  awful.key({ modkey,           }, "j",
+  awful.key({ modkey,           }, "h",
     function ()
       awful.client.focus.byidx(-1)
     end,
@@ -343,9 +343,9 @@ globalkeys = gears.table.join(
     {description = "swap with next client by index", group = "client"}),
   awful.key({ modkey, "Control"   }, "j", function () awful.client.swap.byidx( -1)    end,
     {description = "swap with previous client by index", group = "client"}),
-  awful.key({ modkey,  }, "l", function () awful.screen.focus_relative( 1) end,
+  awful.key({ modkey,  }, "j", function () awful.screen.focus_relative( 1) end,
     {description = "focus the next screen", group = "screen"}),
-  awful.key({ modkey,  }, "h", function () awful.screen.focus_relative(-1) end,
+  awful.key({ modkey,  }, "k", function () awful.screen.focus_relative(-1) end,
     {description = "focus the previous screen", group = "screen"}),
   awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
     {description = "jump to urgent client", group = "client"}),
