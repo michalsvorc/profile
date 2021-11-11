3317f32166ad866062238f77dadc372c04ebea12
Michal Svorc <dev@michalsvorc.com>
2021-11-11 11:18:23 +0100

    nvim: update nightfox colorscheme, custom colors

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -5,17 +5,36 @@ end
 
 -- https://github.com/EdenEast/nightfox.nvim
 
+local fox = 'nordfox'
+local colors = require('nightfox.colors').load(fox)
+
+colors.custom = {
+  cursorline = '#39404c',
+  comment = '#918ba1'
+}
+
 local nightfox = require('nightfox')
 
 nightfox.setup({
-  fox = "nordfox",
+  fox = fox,
   styles = {
     comments = "italic",
-    keywords = "italic",
+    functions = "italic",
+  },
+  colors = {
+    gitSigns = {
+      add    = colors.green_br,
+      change = colors.blue_br,
+      delete = colors.red_br,
+    },
   },
   hlgroups = {
-    TSVariable = { fg = '#cc887c' },
-    Comment = { fg = '#918ba1' },
+    Comment = { fg = colors.custom.comment },
+    CursorLine = { bg = colors.custom.cursorline },
+    NormalFloat = {bg = colors.bg },
+    Pmenu = {bg = colors.bg },
+    TSVariable = { fg = colors.red_br },
+    TSProperty = { fg = colors.blue },
   },
 })
 
