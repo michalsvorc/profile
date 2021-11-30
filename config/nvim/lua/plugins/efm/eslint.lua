26ad9c813c3286744b807cc132967173dd3e55ba
Michal Svorc <dev@michalsvorc.com>
2021-11-30 20:28:48 +0100

    nvim: use global eslint_d instance in efm server

--- config/nvim/lua/plugins/efm/eslint.lua
+++ config/nvim/lua/plugins/efm/eslint.lua
@@ -1,10 +1,10 @@
 -- https://github.com/mantoni/eslint_d.js
 
 return {
-  lintCommand = "yarn eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+  lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
   lintStdin = true,
   lintFormats = {"%f:%l:%c: %m"},
   lintIgnoreExitCode = true,
-  formatCommand = "yarn eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
   formatStdin = true
 }
