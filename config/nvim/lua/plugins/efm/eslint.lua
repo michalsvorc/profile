68032dfcb99ac7a4f37d4db1de8b69b7314786b4
Michal Svorc <dev@michalsvorc.com>
2021-12-02 16:57:43 +0100

    nvim: revert efm lint command to eslint_d

--- config/nvim/lua/plugins/efm/eslint.lua
+++ config/nvim/lua/plugins/efm/eslint.lua
@@ -1,6 +1,10 @@
+-- https://github.com/neovim/nvim-lspconfig/wiki/User-contributed-tips#eslint_d
+
 return {
-  lintCommand = "yarn eslint -f unix --stdin --stdin-filename ${INPUT}",
+  lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
   lintStdin = true,
   lintFormats = {"%f:%l:%c: %m"},
   lintIgnoreExitCode = true,
+  formatCommand = "eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}",
+  formatStdin = true
 }
