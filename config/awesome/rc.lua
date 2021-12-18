a3573cedd034bea1c0c9308de0969ccd79cb48a0
Michal Svorc <dev@michalsvorc.com>
2021-12-18 08:49:50 +0100

    awesomewm: disable border in fullscreen layout

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -547,7 +547,8 @@ end
 clientbuttons = gears.table.join(
   awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
   awful.button({ modkey }, 1, awful.mouse.client.move),
-  awful.button({ modkey }, 3, awful.mouse.client.resize))
+  awful.button({ modkey }, 3, awful.mouse.client.resize)
+)
 
 -- Set keys
 root.keys(globalkeys)
@@ -695,11 +696,12 @@ end)
 
 -- Use borders only on tiled windows.
 screen.connect_signal("arrange", function (s)
-  local max = s.selected_tag.layout.name == "max"
+  local layout_max = s.selected_tag.layout.name == "max"
+  local layout_fullscreen = s.selected_tag.layout.name == "fullscreen"
   local only_one = #s.tiled_clients == 1 -- use tiled_clients so that other floating windows don't affect the count
   -- but iterate over clients instead of tiled_clients as tiled_clients doesn't include maximized windows
   for _, c in pairs(s.clients) do
-    if (max or only_one) and not c.floating or c.maximized then
+    if (layout_max or layout_fullscreen or only_one) and not c.floating or c.maximized then
       c.border_width = 0
     else
       c.border_width = beautiful.border_width
@@ -711,15 +713,15 @@ end)
 -- Plugins
 -- {{{
 --- awesome-switcher
-switcher.settings.preview_box = true                                 -- display preview-box
-switcher.settings.preview_box_bg = "#252525"                         -- background color
-switcher.settings.preview_box_border = "#1b1b1b"                       -- border-color
-switcher.settings.preview_box_fps = 60                               -- refresh framerate
-switcher.settings.preview_box_delay = 75                            -- delay in ms
-switcher.settings.preview_box_title_font = {"sans","italic","normal"}-- the font for cairo
-switcher.settings.preview_box_title_font_size_factor = 1           -- the font sizing factor
-switcher.settings.preview_box_title_color = {189,184,174,1}                -- the font color
-switcher.settings.client_opacity = false                             -- opacity for unselected clients
+switcher.settings.preview_box = true                                  -- display preview-box
+switcher.settings.preview_box_bg = "#252525"                          -- background color
+switcher.settings.preview_box_border = "#1b1b1b"                      -- border-color
+switcher.settings.preview_box_fps = 60                                -- refresh framerate
+switcher.settings.preview_box_delay = 75                              -- delay in ms
+switcher.settings.preview_box_title_font = {"sans","italic","normal"} -- the font for cairo
+switcher.settings.preview_box_title_font_size_factor = 1              -- the font sizing factor
+switcher.settings.preview_box_title_color = {189,184,174,1}           -- the font color
+switcher.settings.client_opacity = false                              -- opacity for unselected clients
 switcher.settings.cycle_raise_client = false                          -- raise clients on cycle
 -- }}}
 
