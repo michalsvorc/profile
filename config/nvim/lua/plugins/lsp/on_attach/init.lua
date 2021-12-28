eb18c68cac7e03303256e022419a0f7b026f9d88
Michal Svorc <dev@michalsvorc.com>
2021-12-28 18:30:32 +0000

    neovim: refactor LSP on_attach lua scripts

---
+++ config/nvim/lua/plugins/lsp/on_attach/init.lua
@@ -0,0 +1,18 @@
+-- Initialize plugins after the language server attaches to the current buffer.
+local icons = require('icons')
+
+return function (client, bufnr)
+  require('plugins.lsp.on_attach.borders')
+  require('plugins.lsp.on_attach.keymap')(client, bufnr)
+  require('plugins.lsp.on_attach.signs')
+  require('plugins.lsp.on_attach.lightbulb')
+  require('illuminate').on_attach(client)
+
+  require('lsp_signature').on_attach({
+      bind = true,
+      hint_prefix = icons.symbols.variable ..' ',
+      handler_opts = {
+        border = "single"
+      }
+    }, bufnr)
+end
