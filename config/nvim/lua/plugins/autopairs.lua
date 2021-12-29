c59d29ebb4eb41d3b83cca031a7b8e9beb88f485
Michal Svorc <dev@michalsvorc.com>
2021-12-29 12:50:41 +0000

    neovim: remove default values from nvim-autopairs

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,5 @@
 -- https://github.com/windwp/nvim-autopairs
 
 require('nvim-autopairs').setup({
-  check_ts = true, --  Use treesitter to check pair.
-  disable_filetype = {"TelescopePrompt"},
-  enable_check_bracket_line = true,
+  check_ts = true, -- Use treesitter to check for a pair
 })
