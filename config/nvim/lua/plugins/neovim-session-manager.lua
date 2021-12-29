91251fd68f9a64175559f77a62531c6499a2506d
Michal Svorc <dev@michalsvorc.com>
2021-12-29 12:55:16 +0000

    neovim: delete unused session plugin scripts

--- config/nvim/lua/plugins/neovim-session-manager.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/Shatur/neovim-session-manager
-
-require('telescope').load_extension('sessions')
-
-require('session_manager').setup({
-  autoload_last_session = false,
-  autosave_last_session = false,
-})
