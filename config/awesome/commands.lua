89c9cb556ab7976fac86823e83210c54f1b9f00f
Michal Svorc <dev@michalsvorc.com>
2022-01-23 10:17:23 +0100

    awesomewm: firefox private flag as private browser

--- config/awesome/commands.lua
+++ config/awesome/commands.lua
@@ -1,7 +1,7 @@
 return {
   browser = 'firefox',
   browser_development = 'firefox-developer',
-  browser_private ='firefox-private',
+  browser_private ='firefox --private-window',
   code='code',
   display='arandr',
   terminal = 'alacritty',
