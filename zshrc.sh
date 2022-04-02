f753cb9def607f9e791286238f75ef9a5c6ebdaf
Michal Svorc <dev@michalsvorc.com>
2022-04-02 17:10:59 +0200

    feat(zsh): Source system zsh-syntax-highlighting

--- zshrc.sh
+++ zshrc.sh
@@ -25,7 +25,7 @@ autoload -U colors && colors
 
 PS1="%{$fg[yellow]%}%n@%m %{$fg[blue]%}%~%{$reset_color%} $%b "
 
-# Git prompt integration
+# Git prompt integration.
 setopt PROMPT_SUBST
 source "$shell_partials_dir/git_prompt.sh"
 
@@ -43,12 +43,12 @@ setopt INC_APPEND_HISTORY \
 # Completion
 #===============================================================================
 
-# Basic auto/tab complete
+# Basic auto/tab complete.
 autoload -U compinit
 zstyle ':completion:*' menu select
 zmodload zsh/complist
 compinit
-_comp_options+=(globdots)		# Include hidden files
+_comp_options+=(globdots)		# Include hidden files.
 
 # Replace zsh's default completion selection menu with fzf.
 # Link: https://github.com/Aloxaf/fzf-tab
@@ -61,14 +61,14 @@ source "$zsh_plugins_dir/fzf-tab/fzf-tab.plugin.zsh"
 bindkey -v
 export KEYTIMEOUT=1
 
-# Use vim keys in tab complete menu
+# Use vim keys in tab complete menu.
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -v '^?' backward-delete-char
 
-# Change cursor shape for different vi modes
+# Change cursor shape for different vi modes.
 function zle-keymap-select {
   if [[ ${KEYMAP} == vicmd ]] ||
      [[ $1 = 'block' ]]; then
@@ -83,19 +83,19 @@ function zle-keymap-select {
 
 zle -N zle-keymap-select
 zle-line-init() {
-    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere).
     echo -ne "\e[5 q"
 }
 zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 
-# Edit line in vim with ctrl-e
+# Edit line in vim with ctrl-e.
 autoload edit-command-line; zle -N edit-command-line
 bindkey '^e' edit-command-line
 
 #===============================================================================
-# Alacritty window title bar
+# Alacritty window title bar.
 # Link: https://wiki.gentoo.org/wiki/Alacritty#Zsh
 #===============================================================================
 
@@ -112,3 +112,10 @@ then
   }
 fi
 
+#===============================================================================
+# Fish shell-like syntax highlighting for Zsh. zsh-syntax-highlighting.zsh must
+# be sourced at the end of the .zshrc file.
+# Link: https://github.com/zsh-users/zsh-syntax-highlighting
+#===============================================================================
+
+source '/usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
