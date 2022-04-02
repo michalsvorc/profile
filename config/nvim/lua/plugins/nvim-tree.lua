6eacc2843f7b55493cb29cd63facd7d7fcb1d0de
Michal Svorc <dev@michalsvorc.com>
2022-04-02 22:37:33 +0200

    nvim: fix nvim-tree plugin
    - remove deprecated configs
    - remove default settings

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -6,7 +6,6 @@ vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent mar
 vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_symlink_arrow = icons.file.symlink -- used as a separator between symlinks' source and target.
-vim.g.nvim_tree_disable_window_picker = 1 -- 0 by default, will disable the window picker.
 
 vim.g.nvim_tree_icons = {
   default = icons.file.default,
@@ -33,32 +32,9 @@ vim.g.nvim_tree_icons = {
 }
 
 require('nvim-tree').setup {
-  -- disables netrw completely
-  disable_netrw       = false,
-  -- hijack netrw window on startup
-  hijack_netrw        = false,
-  -- open the tree when running this setup function
-  open_on_setup       = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close          = true,
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
-  -- hijacks new directory buffers when they are opened.
-  update_to_buf_dir   = {
-    -- enable the feature
-    enable = false,
-    -- allow to open the tree if it was previously closed
-    auto_open = false,
-  },
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
-  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = false,
-  -- show lsp diagnostics in the signcolumn
+  hijack_netrw = false,
   diagnostics = {
-    enable = false,
+    enable = false, -- show lsp diagnostics in the signcolumn
     icons = {
       hint = icons.diagnostics.hint,
       info = icons.diagnostics.info,
@@ -66,41 +42,10 @@ require('nvim-tree').setup {
       error = icons.diagnostics.error,
     }
   },
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
-    -- enables the feature
-    enable      = true,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = false,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {'.git', 'node_modules', '.cache'}
-  },
-  -- configuration options for the system open command (`s` in the tree by default)
-  system_open = {
-    -- the command to run this, leaving nil should work in most cases
-    cmd  = nil,
-    -- the command arguments as a list
-    args = {}
-  },
-
   view = {
-    -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-    width = 35,
-    -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+    width = 45,
     height = 30,
-    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
     side = 'right',
-    -- if true the tree will resize itself after opening a file
-    auto_resize = false,
-    mappings = {
-      -- custom only false will merge the list with the default mappings
-      -- if true, it will only use your list to set the mappings
-      custom_only = false,
-      -- list of mappings to set on the tree manually
-      list = {}
-    }
   }
 }
 
