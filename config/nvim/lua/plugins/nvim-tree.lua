ae52a036486b0af19ea1ba8e21f342119a7e3a3c
Michal Svorc <dev@michalsvorc.com>
2022-04-18 09:36:03 +0200

    nvim: update nvim-tree indent markers configuration

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -2,7 +2,6 @@
 
 local icons = require('icons')
 
-vim.g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 vim.g.nvim_tree_symlink_arrow = icons.file.symlink -- used as a separator between symlinks' source and target.
@@ -46,6 +45,11 @@ require('nvim-tree').setup {
     width = 45,
     height = 30,
     side = 'right',
-  }
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
+    },
+  },
 }
 
