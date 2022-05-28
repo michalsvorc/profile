26b1ccf1ac5aa8316d055ae062050c027118cc72
Michal Svorc <dev@michalsvorc.com>
2022-05-28 10:40:33 +0200

    feat(scripts): lf and vim local binary install

---
+++ scripts/lf.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#
+# Application: LF
+# Description: Application install script.
+# Releases: https://github.com/gokcehan/lf/releases
+#
+# Dependencies: curl, jq
+#
+# Author: Michal Svorc <dev@michalsvorc.com>
+# License: MIT license (https://opensource.org/licenses/MIT)
+
+#===============================================================================
+# Abort the script on errors and unbound variables
+#===============================================================================
+
+set -o errexit      # Abort on nonzero exit status.
+set -o nounset      # Abort on unbound variable.
+set -o pipefail     # Don't hide errors within pipes.
+# set -o xtrace       # Set debugging.
+
+#===============================================================================
+# Variables
+#===============================================================================
+
+readonly repository_id='gokcehan/lf'
+readonly asset='lf-linux-amd64.tar.gz'
+readonly executable_dir="${HOME}/.local/bin"
+readonly repository_uri="https://api.github.com/repos/${repository_id}/releases"
+
+#===============================================================================
+# Functions
+#===============================================================================
+
+get_release_metadata() {
+  local repository_uri="$1"
+
+  printf '%s' $(\
+    jq -r ".[0]" \
+    <<< $(curl "$repository_uri")  \
+  )
+}
+
+parse_tag_name() {
+  local release_metadata="$1"
+
+  printf '%s' $(\
+    jq -r ".tag_name" \
+    <<< "$release_metadata"  \
+  )
+}
+
+parse_download_uri() {
+  local release_metadata="$1"
+
+  printf '%s' $(\
+    jq -r ".assets \
+    | map(select(.name==\"${asset}\"))[0] \
+    | .browser_download_url" \
+    <<< "$release_metadata"  \
+  )
+}
+
+#===============================================================================
+# Execution
+#===============================================================================
+
+release_metadata=$(get_release_metadata "$repository_uri")
+tag_name=$(parse_tag_name "$release_metadata")
+download_uri=$(parse_download_uri "$release_metadata")
+
+mkdir -p "$executable_dir" \
+  && cd $_ \
+  && curl -Lo "$asset" "$download_uri" \
+  && tar -xvf "$asset" -C . \
+  && rm "$asset"
+
---
+++ scripts/nvim.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+# Application: Neovim
+# Description: Application install script.
+# Releases: https://github.com/neovim/neovim/releases
+#
+# Dependencies: curl, jq
+#
+# Author: Michal Svorc <dev@michalsvorc.com>
+# License: MIT license (https://opensource.org/licenses/MIT)
+
+#===============================================================================
+# Abort the script on errors and unbound variables
+#===============================================================================
+
+set -o errexit      # Abort on nonzero exit status.
+set -o nounset      # Abort on unbound variable.
+set -o pipefail     # Don't hide errors within pipes.
+# set -o xtrace       # Set debugging.
+
+#===============================================================================
+# Variables
+#===============================================================================
+
+readonly repository_id='neovim/neovim'
+readonly tag_name='nightly'
+readonly target_system='nvim-linux64'
+readonly asset="${target_system}.tar.gz"
+readonly local_dir="${HOME}/.local"
+readonly executable_dir="${local_dir}/bin"
+readonly repository_uri="https://api.github.com/repos/${repository_id}/releases"
+
+#===============================================================================
+# Functions
+#===============================================================================
+
+get_release_metadata() {
+  local repository_uri="$1"
+  local tag_name="$2"
+
+  printf '%s' $(\
+    jq -r ".[] | select(.tag_name==\"${tag_name}\")" \
+    <<< $(curl "$repository_uri")  \
+  )
+}
+
+parse_download_uri() {
+  local release_metadata="$1"
+
+  printf '%s' $(\
+    jq -r ".assets \
+    | map(select(.name==\"${asset}\"))[0] \
+    | .browser_download_url" \
+    <<< "$release_metadata"  \
+  )
+}
+
+#===============================================================================
+# Execution
+#===============================================================================
+
+release_metadata=$(get_release_metadata "$repository_uri" "$tag_name")
+download_uri=$(parse_download_uri "$release_metadata")
+
+mkdir -p "$executable_dir" \
+  && cd "$local_dir" \
+  && curl -Lo "$asset" "$download_uri" \
+  && tar -xvf "$asset" -C . \
+  && rm "$asset" \
+  && cd "$executable_dir" \
+  && ln -sf "${local_dir}/${target_system}/bin/nvim" 'nvim'
+
