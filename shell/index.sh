422ebcb9cba14d9c479b33c9216db40a82f7337b
Michal Svorc <dev@michalsvorc.com>
2022-12-29 21:52:29 +0100

    feat(nnn): add nnn_quitcd function and alias

--- shell/index.sh
+++ shell/index.sh
@@ -5,10 +5,12 @@
 shell_config_dir="${HOME}/.local/profile/shell"
 
 source "${shell_config_dir}/term.sh"
-source "${shell_config_dir}/aliases.sh"
+
 source "${shell_config_dir}/bash_loader.sh"
 source "${shell_config_dir}/history.sh"
 source "${shell_config_dir}/plugins/history_search.sh"
 source "${shell_config_dir}/plugins/lf.sh"
+source "${shell_config_dir}/plugins/nnn_quitcd.sh"
 source "${shell_config_dir}/plugins/ssh_agent.sh"
 
+source "${shell_config_dir}/aliases.sh"
