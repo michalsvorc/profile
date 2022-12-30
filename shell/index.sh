4d13a1cfa95038f217c4fbbbb40a80cd85c01f92
Michal Svorc <dev@michalsvorc.com>
2022-12-30 10:42:47 +0100

    feat(bash): drop support, delete scripts

--- shell/index.sh
+++ shell/index.sh
@@ -6,7 +6,6 @@ shell_config_dir="${HOME}/.local/profile/shell"
 
 source "${shell_config_dir}/term.sh"
 
-source "${shell_config_dir}/bash_loader.sh"
 source "${shell_config_dir}/history.sh"
 source "${shell_config_dir}/plugins/history_search.sh"
 source "${shell_config_dir}/plugins/lf.sh"
