0e6431fca152e708b155410ea4b28baccbb7849a
Michal Svorc <dev@michalsvorc.com>
2022-12-31 19:47:58 +0100

    refactor(shell): streamline git prompt

--- shell/git_prompt.sh
+++ shell/git_prompt.sh
@@ -1,26 +1,24 @@
 #===============================================================================
 # Git prompt integration
+#
 # Link: https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
+# Dependencies: git
 #===============================================================================
 
-source_git_prompt() {
-  local script_path=$1
+readonly script_name='git-prompt.sh'
+readonly script_path_system="/usr/share/git/${script_name}"
+readonly script_path_user="${HOME}/.local/share/git/${script_name}"
 
-  source $script_path \
-    && export GIT_PS1_SHOWCOLORHINTS=true \
-    && export GIT_PS1_SHOWDIRTYSTATE=true \
-    && export GIT_PS1_SHOWUNTRACKEDFILES=true \
-    && export GIT_PS1_SHOWUPSTREAM="auto"
-
-  PS1=$PS1'$(__git_ps1 "(%s) ")'
-}
-
-script_path_system='/usr/share/git/git-prompt.sh'
-script_path_user="$HOME/.local/share/git/git-prompt.sh"
-
-if [[ -e $script_path_system ]]; then
-  source_git_prompt $script_path_system
-elif [[ -e $script_path_user ]]; then
-  source_git_prompt $script_path_user
+if [[ -e "$script_path_system" ]]; then
+  script_path="$script_path_system"
+elif [[ -e "$script_path_user" ]]; then
+  script_path="$script_path_user"
 fi
 
+source $script_path \
+  && export GIT_PS1_SHOWCOLORHINTS=true \
+  && export GIT_PS1_SHOWDIRTYSTATE=true \
+  && export GIT_PS1_SHOWUNTRACKEDFILES=true \
+  && export GIT_PS1_SHOWUPSTREAM="auto"
+
+PS1=$PS1'$(__git_ps1 "(%s) ")'
