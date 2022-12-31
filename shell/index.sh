83c0bbfd3bf3ce64b767b7cc8b8a9db1001a7592
Michal Svorc <dev@michalsvorc.com>
2022-12-31 19:45:58 +0100

    feat(init): remove lf link

--- shell/index.sh
+++ shell/index.sh
@@ -2,10 +2,9 @@
 # Shared shell configuration sourced by interactive and login shells
 #===============================================================================
 
-shell_config_dir="${HOME}/.local/profile/shell"
+readonly shell_config_dir="${HOME}/.local/profile/shell"
 
 source "${shell_config_dir}/history_search.sh"
-source "${shell_config_dir}/lf.sh"
 source "${shell_config_dir}/nnn_quitcd.sh"
 source "${shell_config_dir}/ssh_agent.sh"
 
