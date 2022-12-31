a6bdcc4e97557cd64778548e28b5e591e0e28270
Michal Svorc <dev@michalsvorc.com>
2022-12-31 20:54:56 +0100

    chore(zshrc): move syntax highlighting at the end

--- shell/syntax_hlg.sh
+++ shell/syntax_hlg.sh
@@ -1,16 +1,16 @@
 #===============================================================================
-# Fish shell-like syntax highlighting for Zsh. zsh-syntax-highlighting.zsh must
-# be sourced at the end of the .zshrc file.
+# Fish shell-like syntax highlighting for Zsh.
+#
 # Link: https://github.com/zsh-users/zsh-syntax-highlighting
 #===============================================================================
 
-zsh_syntax_highlighting='zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
+file_path='zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
 
-if [[ -f "/usr/local/share/${zsh_syntax_highlighting}" ]]; then
-  source "/usr/local/share/${zsh_syntax_highlighting}"
-elif [[ -f "/usr/share/${zsh_syntax_highlighting}" ]]; then
-  source "/usr/share/${zsh_syntax_highlighting}"
-elif [[ -f "/opt/homebrew/opt/zsh-syntax-highlighting/share/${zsh_syntax_highlighting}" ]]; then
-  source "/opt/homebrew/opt/zsh-syntax-highlighting/share/${zsh_syntax_highlighting}"
+if [[ -f "/usr/local/share/${file_path}" ]]; then
+  source "/usr/local/share/${file_path}"
+elif [[ -f "/usr/share/${file_path}" ]]; then
+  source "/usr/share/${file_path}"
+elif [[ -f "/opt/homebrew/opt/zsh-syntax-highlighting/share/${file_path}" ]]; then
+  source "/opt/homebrew/opt/zsh-syntax-highlighting/share/${file_path}"
 fi
 
