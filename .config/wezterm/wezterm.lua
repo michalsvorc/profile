1c8570b917207d568f4545ff7e76e5b3d09efb73
Michal Svorc <m.svorc@sportradar.com>
2023-03-27 12:28:30 +0200

    refactor(wezterm): use newer config_builder

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,28 @@
 -- Configuration: https://wezfurlong.org/wezterm/config/files.html
--- Color schemes: https://github.com/mbadolato/iTerm2-Color-Schemes
+-- Colors & Appearance: https://wezfurlong.org/wezterm/config/appearance.html
+-- Color Schemes: https://wezfurlong.org/wezterm/colorschemes/index.html
 
-local wezterm = require 'wezterm';
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
 
-return {
-  color_scheme = "OceanicMaterial",
-  font = wezterm.font("Hack Nerd Font"),
-  font_size = 16,
-  cursor_blink_rate = 0,
-  exit_behavior = "Close",
-  audible_bell = "Disabled"
-}
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Apply your config choices
+config.color_scheme = "tokyonight-storm"
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 14
+config.cursor_blink_rate = 0
+config.exit_behavior = "Close"
+config.audible_bell = "Disabled"
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Return the configuration to wezterm
+return config
+
