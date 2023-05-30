f36a058e1a1d69ddabb2526b8eaa93aa985b5867
Michal Svorc <dev@michalsvorc.com>
2023-05-30 11:26:07 +0200

    refactor(scripts): extract functions in update

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,13 +4,34 @@ readonly script_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 readonly plugins_dir="${script_dir}/../shell/plugins"
 
 #===============================================================================
+# Functions
+#===============================================================================
+
+construct_target() {
+  local source="$1"
+
+  printf '%s' "${plugins_dir}/$(basename $source)"
+}
+
+download_script() {
+  local source="$1"
+  local target="$2"
+
+  printf 'Downloading %s\n' "$(basename $source)"
+
+  curl -JL "$source" -o "$target"
+}
+
+#===============================================================================
 # Git prompt
 # https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
 #===============================================================================
 
 source='https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh'
 target="${plugins_dir}/$(basename $source)"
-curl -JL "$source" -o "$target"
+target2=$(construct_target "$source")
+
+download_script "$source" "$target"
 
 #===============================================================================
 # ohmyzsh vi mode
@@ -19,5 +40,6 @@ curl -JL "$source" -o "$target"
 
 source='https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/vi-mode/vi-mode.plugin.zsh'
 target="${plugins_dir}/$(basename $source)"
-curl -JL "$source" -o "$target"
+target2=$(construct_target "$source")
 
+download_script "$source" "$target"
