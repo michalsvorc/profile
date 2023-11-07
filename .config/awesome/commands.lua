faaa0bdfa1d72ec5d8d7d21175e443429ac3839c
Michal Svorc <dev@michalsvorc.com>
2023-11-07 18:53:59 +0100

    chore(awesome): remove configuration
    Moved to separate git repository.

--- .config/awesome/commands.lua
+++
@@ -1,9 +0,0 @@
-return {
-  browser = 'firefox',
-  browser_development = 'firefox-developer',
-  browser_private ='firefox --private-window',
-  code='code',
-  display='arandr',
-  screenshot='import tmp/screenshot.png',
-  terminal = 'alacritty',
- }
--- scripts/init.sh
+++ scripts/init.sh
@@ -92,8 +92,6 @@ link_home() {
 link_config() {
   dir='alacritty'
   create_symlink "${profile_config_dir}/${dir}" "${XDG_CONFIG_HOME}/${dir}"
-  dir='awesome'
-  create_symlink "${profile_config_dir}/${dir}" "${XDG_CONFIG_HOME}/${dir}"
   dir='lazygit'
   create_symlink "${profile_config_dir}/${dir}/config.yml" "${XDG_CONFIG_HOME}/${dir}/config.yml"
   dir='nvim'
