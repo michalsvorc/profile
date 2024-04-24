4e08b6f7cac6c08395081a0c8dd3376b42bfe36d
Michal Svorc <dev@michalsvorc.com>
2024-04-24 16:33:38 +0200

    feat(shell): add cd to ancestor function

--- .zshrc
+++ .zshrc
@@ -81,9 +81,10 @@ source "${plugins_dir}/zsh/vi-mode.plugin.zsh"
 source "${shell_dir}/ssh_agent.sh"
 
 #===============================================================================
-# Helper functions
+# Change directory functions
 #===============================================================================
 
+source "${shell_dir}/cd/ancestor.sh"
 source "${shell_dir}/cd_up.sh"
 
 #===============================================================================
---
+++ shell/cd/ancestor.sh
@@ -0,0 +1,20 @@
+#===============================================================================
+# Change directory to ancestor
+# https://www.reddit.com/r/commandline/comments/1cbj6sg/simple_cli_utility_to_cd_to_a_particular_ancestor/
+#
+# Dependencies: cat
+#===============================================================================
+cdanc() {
+  cmd="cat"
+    [[ $1 == -r ]] && { cmd=tac; shift; }
+    occ=1
+    [[ $2 =~ ^[0-9]+$ ]] && occ=$2
+
+    p="${PWD}"
+    cd "$(
+        while [[ $p != "${HOME}" ]]; do
+            p=${p%/*}
+            echo "${p}"
+        done | grep -E "$1[^/]*$" | $cmd | sed -ne "${occ}p"
+    )" || return
+}
