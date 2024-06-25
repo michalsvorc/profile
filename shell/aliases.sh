6687bed4edd4ffcab7a34b94bbfd4954b63ccdae
Michal Svorc <dev@michalsvorc.com>
2024-06-25 15:20:54 +0200

    feat(aliases): e for editor, f for file explorer

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -30,17 +30,19 @@ alias docker_start='rc-service docker start'
 alias pass='EDITOR=$EDITOR_SIMPLE pass'
 
 # Custom
+alias e="$EDITOR"
+alias f='yazi'
+alias g='git'
+alias t='tmux -u2'
+
 alias aws_prompt='aws --cli-auto-prompt'
-alias ed="nvim"
 alias ftp='ncftp'
 alias ll='eza --all --git --group --group-directories-first --icons --long --time-style=long-iso'
 alias mount_as_user='mount -o umask=0022,gid=$(id -g),uid=$(id -u)'
 alias npr='npm run'
-alias t='tmux -u2'
 alias temp='cat /sys/class/thermal/thermal_zone*/temp'
 alias qrdecode='zbarimg'
 alias qrencode='qrencode -t ASCIIi'
 alias uid='lazydocker'
 alias uig='nvim -c "tab Git"'
-alias fe='yazi'
 alias screenshot='import $HOME/tmp/screenshot.png'
