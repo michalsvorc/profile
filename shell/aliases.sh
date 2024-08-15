9d3b397a6b05165d3f9f55866f84de0cfd1abd7a
Michal Svorc <dev@michalsvorc.com>
2024-08-15 23:50:59 +0200

    chore(aliases): change snake_case to kebab-case

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -5,7 +5,7 @@
 # Options
 alias bat='bat --plain'
 alias ls='ls -alF'
-alias xclip_c='xclip -selection c'
+alias xclip-c='xclip -selection c'
 
 # Sudo
 alias cryptsetup='sudo cryptsetup'
@@ -34,7 +34,7 @@ alias t='tmux -u2'
 alias aws_prompt='aws --cli-auto-prompt'
 alias ftp='ncftp'
 alias ll='eza --all --git --group --group-directories-first --icons --long --time-style=long-iso'
-alias mount_as_user='mount -o umask=0022,gid=$(id -g),uid=$(id -u)'
+alias mount-as-user='mount -o umask=0022,gid=$(id -g),uid=$(id -u)'
 alias temp='cat /sys/class/thermal/thermal_zone*/temp'
 alias qrdecode='zbarimg'
 alias qrencode='qrencode -t ASCIIi'
