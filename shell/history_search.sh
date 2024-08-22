818afdab031f1f776beb4cc59d4e048d58fb7793
Michal Svorc <dev@michalsvorc.com>
2024-08-22 04:59:56 +0200

    feat(history): add copy to clipboard

--- shell/history_search.sh
+++ shell/history_search.sh
@@ -1,13 +1,22 @@
 #===============================================================================
 # Search and execute command from shell history file
 #
-# Dependencies: fzf, sed
+# Dependencies: fzf, sed, clipboard
 #===============================================================================
 
 hiss() {
+  clipboard_command="xclip -selection clipboard"
+
+  # Detect if running on macOS
+  if [[ "$(uname)" == "Darwin" ]]; then
+    clipboard_command="pbcopy"
+  fi
+
   eval "$(
     history 1 |
-      fzf +s --tac |
+      fzf +s --tac \
+        --bind="ctrl-c:execute-silent(echo {} | sed 's/ *[0-9]* *//' | tr -d '\n' | $clipboard_command)" \
+        --header='CTRL-C to copy to clipboard, ENTER to execute' |
       sed 's/ *[0-9]* *//'
   )"
 }
