3216d8426548c8bfb3bda4de7b9bab03166e0096
Michal Svorc <dev@michalsvorc.com>
2024-09-08 19:57:21 +0200

    feat(fzf): preview for stat command

--- shell/fzf/tab-completion.sh
+++ shell/fzf/tab-completion.sh
@@ -9,3 +9,4 @@ preview_file='eval $CMD_PAGER {1}'
 zstyle ':completion::*:*' fzf-completion-opts --preview="[[ -d {2} ]] && ${preview_dir} || ([[ -f {2} ]] && eval ${preview_file})"
 zstyle ':completion::*:cd::*' fzf-completion-opts --preview="${preview_dir}"
 zstyle ':completion::*:git::git,add,*' fzf-completion-opts --preview='git -c color.status=always status --short'
+zstyle ':completion::*:stat::*' fzf-completion-opts --preview='stat {2}'
