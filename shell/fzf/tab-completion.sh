857cf9d8c254ace2a0dce7752cc7498ab435d011
Michal Svorc <dev@michalsvorc.com>
2024-09-08 19:41:50 +0200

    feat(fzf): default preview for files and dirs

--- shell/fzf/tab-completion.sh
+++ shell/fzf/tab-completion.sh
@@ -3,6 +3,9 @@
 # https://github.com/lincheney/fzf-tab-completion?tab=readme-ov-file#specifying-custom-fzf-options
 #===============================================================================
 
-zstyle ':completion::*:cd::*' fzf-completion-opts --preview='eval $CMD_LIST_DIR --tree {1}'
-zstyle ':completion::*:bat::*' fzf-completion-opts --preview='eval $CMD_PAGER {1}'
+preview_dir='eval $CMD_LIST_DIR --tree {1}'
+preview_file='eval $CMD_PAGER {1}'
+
+zstyle ':completion::*:*' fzf-completion-opts --preview="[[ -d {2} ]] && ${preview_dir} || ([[ -f {2} ]] && eval ${preview_file})"
+zstyle ':completion::*:cd::*' fzf-completion-opts --preview="${preview_dir}"
 zstyle ':completion::*:git::git,add,*' fzf-completion-opts --preview='git -c color.status=always status --short'
