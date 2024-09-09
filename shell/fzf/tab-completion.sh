4fcb6d01306e0bb6ff6f35ea1285a61e3428900b
Michal Svorc <dev@michalsvorc.com>
2024-09-09 15:22:06 +0200

    feat(fzf): depth of 2 for directory tree view

--- shell/fzf/tab-completion.sh
+++ shell/fzf/tab-completion.sh
@@ -3,7 +3,7 @@
 # https://github.com/lincheney/fzf-tab-completion?tab=readme-ov-file#specifying-custom-fzf-options
 #===============================================================================
 
-preview_dir='eval $CMD_LIST_DIR --tree {1}'
+preview_dir='eval $CMD_LIST_DIR --tree --level 2 {1}'
 preview_file='eval $CMD_PAGER {1}'
 
 zstyle ':completion::*:*' fzf-completion-opts --preview="[[ -d {2} ]] && ${preview_dir} || ([[ -f {2} ]] && eval ${preview_file})"
