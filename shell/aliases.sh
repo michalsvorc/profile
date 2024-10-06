8c5c6e764140b0d4e109588078b76fba4ff0dee1
Michal Svorc <dev@michalsvorc.com>
2024-10-06 20:47:23 +0200

    chore(aliases): change git client to "gg"

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -33,7 +33,7 @@ alias t='tmux -u2'
 alias aws_prompt='aws --cli-auto-prompt'
 alias fe="nvim -c \"lua require('oil').open()\""
 alias ftp='ncftp'
-alias gi="git-client"
+alias gg="git-client"
 alias ll="$CMD_LIST_DIR"
 alias mount-as-user='mount -o umask=0022,gid=$(id -g),uid=$(id -u)'
 alias temp='cat /sys/class/thermal/thermal_zone*/temp'
