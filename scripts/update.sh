2e28b1d355f2d42ba6d54eb9c3252d0a930d3533
Michal Svorc <dev@michalsvorc.com>
2024-10-16 09:23:33 +0200

    chore(scripts): clean update.sh of unused functions

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
 
-script_dir=$(
-  cd "$(dirname "${BASH_SOURCE[0]}")" || exit
-  pwd -P
-)
-readonly script_dir
-readonly plugins_dir="${script_dir}/../plugins"
-
-#===============================================================================
-# Functions
-#===============================================================================
-
-download_script() {
-  local source="$1"
-  local target="$2"
-
-  printf 'Downloading %s\n' "$(basename "$source")"
-
-  curl -JL "$source" -o "$target"
-}
-
 #===============================================================================
 # Git submodules
 #===============================================================================
