9b4783ed5ba197a332d5e2bf7b07fc45bbf73914
Michal Svorc <dev@michalsvorc.com>
2024-10-18 11:06:07 +0200

    chore(shell): rename cd-up to cdup, maintenance

--- shell/cd/up.sh
+++ shell/cd/up.sh
@@ -1,11 +1,11 @@
 #===============================================================================
-# Change directory to one of the searchable parent directory.
+# Change directory to one of the parent directories.
 # https://www.reddit.com/r/commandline/comments/1cbj6sg/simple_cli_utility_to_cd_to_a_particular_ancestor/
 #
 # Dependencies: cat, fzf, grep
 #===============================================================================
 
-cd-up() {
+cdup() {
   local cmd="cat"
   if [[ $1 == "-r" ]]; then
     cmd="tac"
@@ -14,7 +14,8 @@ cd-up() {
 
   local p="${PWD}"
 
-  local target_dir=$(while [[ $p != "${HOME}" ]]; do
+  local target_dir
+  target_dir=$(while [[ $p != "${HOME}" ]]; do
     p=${p%/*}
     echo "${p}"
   done | grep -E "$1[^/]*$" | $cmd | fzf --reverse --inline-info)
